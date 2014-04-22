da3e458b0171805fed9d20fd338abc0f7c65de3c
CCrosbyweb <celeste@eagles.ewu.edu>
2014-04-21 23:04:32 -0700

    version 3

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,24 @@ $(document).foundation();
 
 // go Motal!
 $('#myModal').foundation('reveal', 'open');
+// changing  welcome Quote
+// Changing BlockQuote and Header Img
+		var limit = 3;
+		var numRand = Math.floor(Math.random()*limit);
 
+		
+		// store names of CSS classes
+		var bg0 = 'bg0';
+		var bg1 = 'bg1';
+		var bg2 = 'bg2';
+
+		//welcome messages
+		var msg0 = '"The beautiful thing about learning is noboy can take it away from you" - B.B King';
+		var msg1 = '"Education is the most powerful weapon which you can use to change the world" - B.B King';
+		var msg2 = '"Anyone who stops learning is old, whether at twenty or eighty. Anyone who keeps learning stays young" -B.B King';
+
+
+		// concatenate 'bg' plus number between 0-to-2
+		$('header div').addClass('bg' + numRand);
+		$('blockquote').text( eval('msg' + numRand) );
+	
